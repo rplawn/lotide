e21d0fe5c909af2ea8018b8d3a5f33a884ebc07b
roop <rplawn@icloud.com>
2022-03-27 22:26:25 -0700

    Return subset of array, removing unwanted items

---
+++ without.js
@@ -0,0 +1,43 @@
+//eqArrays function
+const eqArrays = function (Array1, Array2) {
+  if (Array.isArray(Array1) &&
+  Array.isArray(Array2)) {
+    for (const [i, value] of Array1.entries()) {
+      const value2 = Array2[i]
+        if (value !== value2){
+          return false;
+        }
+      }
+    
+  } return true;
+};
+
+
+//assertArraysEqual function
+const assertArraysEqual = function(Array1, Array2) {
+  if (eqArrays(Array1, Array2)) {
+    console.log(`Assertion Passed: ${Array1} âœ…âœ…âœ… ${Array2}`);
+  } else {
+    console.log(`Assertion Failed: ${Array1} ðŸ›‘ðŸ›‘ðŸ›‘ ${Array2}`);
+  }
+};
+
+
+//Implement without which will return a subset of a given array, removing unwanted elements.
+const without = function (Array1, Array2) {
+  if (Array.isArray(Array1) &&
+  Array.isArray(Array2)) {
+    let newArray1 = [...Array1];
+    for (const [i, value] of newArray1.entries()) {
+      for (const [j, value2] of Array2.entries()){
+        if (value === value2){
+          newArray1.splice(i,1)
+        }
+      }
+    }
+    return newArray1;
+    } else { console.log("Both are not arrays.");
+  }
+};
+
+console.log(without([1,2,3],[1]));
