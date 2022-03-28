263a11bb7f64683f43d95c958880ec54646cb76c
roop <rplawn@icloud.com>
2022-03-27 22:22:04 -0700

    function that compares two arrays and returns true or false

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+//assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} âœ…âœ…âœ… ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} ðŸ›‘ðŸ›‘ðŸ›‘ ${expected}`);
+  }
+};
+
+
+//Takes in two arrays and returns true or false, based on a perfect match.
+const eqArrays = function (Array1, Array2) {
+  if (Array.isArray(Array1) &&
+  Array.isArray(Array2)) {
+    for (const [i, value] of Array1.entries()) {
+      const value2 = Array2[i]
+        if (value !== value2){
+          return false;
+        }
+      }
+    
+  } return true;
+};
+
+console.log(eqArrays([1,4],[1,3]));
+
+assertEqual(1,3);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1], [1]), true);
+assertEqual(eqArrays(["test"], ["test", "ing"]), true);
