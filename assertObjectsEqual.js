4d4440a04f866ee6e8f53b7dd8e4d73f0516f1ab
roop <rplawn@icloud.com>
2022-04-08 23:34:59 -0700

    compare two objects and pass/fail accordingly

---
+++ assertObjectsEqual.js
@@ -0,0 +1,8 @@
+const assertObjectsEquals = function(actual, expected) {
+  const inspect = require('util').inspect; 
+  if (eqObjects(actual, expected)) {
+    console.log(`Assertion Passed: ${inspect(actual)} ✅✅✅ ${inspect(expected)}`);
+  } else {
+    console.log(`Assertion Failed: ${inspect(actual)} 🛑🛑🛑 ${inspect(expected)}`);
+  }
+};
