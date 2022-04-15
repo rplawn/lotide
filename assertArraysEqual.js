0b277ad8cd80e4fa0bfdc16f496984d1c6b1906f
roop <rplawn@icloud.com>
2022-04-14 21:27:42 -0700

    refactored assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,6 @@
-//eqArrays function
-const eqArrays = function(Array1, Array2) {
-  if (Array.isArray(Array1) &&
-  Array.isArray(Array2)) {
-    for (const [i, value] of Array1.entries()) {
-      const value2 = Array2[i];
-      if (value !== value2) {
-        return false;
-      }
-    }
-    
-  } return true;
-};
+const eqArrays = require(`./eqArrays`);
 
-
-//Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console. Instead of doing a simple === check though, this assertion function will make use of your eqArrays function for array comparison.
+// assertArraysEqual function
 const assertArraysEqual = function(Array1, Array2) {
   if (eqArrays(Array1, Array2)) {
     console.log(`Assertion Passed: ${Array1} ✅✅✅ ${Array2}`);
@@ -21,3 +8,5 @@ const assertArraysEqual = function(Array1, Array2) {
     console.log(`Assertion Failed: ${Array1} 🛑🛑🛑 ${Array2}`);
   }
 };
+
+module.exports = assertArraysEqual;
