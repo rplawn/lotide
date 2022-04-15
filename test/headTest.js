34aeb3a7d9876c330d18e27ec201e9ab93ebf2e1
roop <rplawn@icloud.com>
2022-04-14 22:07:05 -0700

    converted headTest.js to use Mocha and Chai tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,18 @@
-const head = require(`../head`);
+//const head = require(`../head`);
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-//Test code
-assertEqual(head([7,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+
+//Mocha & Chai test
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 'hello' for ['hello', 'test']", () => {
+    assert.strictEqual(head(['hello', 'test']), 'hello'); 
+  });
+});
