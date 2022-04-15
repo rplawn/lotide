ad388c6984aecd0706426d57d9b8653cc4d833cf
roop <rplawn@icloud.com>
2022-04-14 21:13:35 -0700

    refactored tail function

--- tail.js
+++ tail.js
@@ -1,23 +1,9 @@
-//assertEqual function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} ✅✅✅ ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} 🛑🛑🛑 ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-
-//Create a function tail which returns the "tail" of an array: everything except for the first item (head) of the provided array
-//Note* Splice changes original array and slice doesn't//
+// tail function
 const tail = function(array) {
   return array.slice(1);
 };
 
+module.exports = tail;
 
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(tail(words));
