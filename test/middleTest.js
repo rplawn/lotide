025c4120935ec1b25989f2386f947d3deb7074bd
roop <rplawn@icloud.com>
2022-04-19 18:42:34 -0700

    fixed mocha & chai testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -4,7 +4,7 @@ const assert = require('chai').assert;
 
 //Mocha & Chai Test
 describe("#middle", () => {
-  it("returns [4,3] for [1,2,3,4,5,6]", () => {
-    assert.strictEqual(middle([1,2,3,4,5,6]), [4,3]); 
+  it("returns [4, 3] for [1,2,3,4,5,6]", () => {
+    assertArraysEqual(middle([1,2,3,4,5,6]), [4,3]); 
   });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -6,6 +6,6 @@ const assert = require('chai').assert;
 //Mocha & Chai Test
 describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); 
+    assertEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); 
   });
 });
